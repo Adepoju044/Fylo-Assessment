01cc4e6fe8e69185d5fdfb7ee55ca0892f2c8f84
Adepoju <adepojufathia04@gmail.com>
2025-03-13 13:46:27 +0100

    fylo assessment

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '384px',
+      md: '768px',
+      lg: '968px',
+      xl: '1440px',
+    },
+    extend: {
+      colors:{
+      VeryDarkBlue: 'hsl(243, 87%, 12%)',
+      DesaturatedBlue: 'hsl(238, 22%, 44%)',
+      BrightBlue: 'hsl(224, 93%, 58%)',
+      ModerateCyan: 'hsl(170, 45%, 43%)',
+      LightGrayishBlue: 'hsl(240, 75%, 98%)',
+      LightGray: 'hsl(0, 0%, 75%)',
+      boxShadow: '5px, 5px, 10px rgba(0px, 0px, 0px, 0.15)',
+      brightbluedark: 'hsl(224, 93%, 38%)',
+      moderatecyandark: 'hsl(170, 45%, 23%)',
+      },
+  },
+  plugins: [],
+  },
+}
